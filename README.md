8d71c866b783a8f714ca787c3bd847e26ea68997
Randall Seethe <rseed42@gmail.com>
2013-07-30 19:22:02 +0200

    set up package.el

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
---- Notes --
-
-This my personal Emacs setup.
-
+===== Notes About This Configuration ===========================================
 First things first:
-
  Replace ~/.emacs with a link to .emacs.d/init.el
+
++------------------------------------------------------------------------------+
+| Built-in Functions                                                           |
++------------------------------------------------------------------------------+
+1. Disable automatic backups
+2. Add .emacs.d/local to the load-path
+3. Load package.el (which itself is copied in local)
+
++------------------------------------------------------------------------------+
+| Installed Packages                                                           |
++------------------------------------------------------------------------------+
+1. Magit
--- init.el
+++ init.el
@@ -1,4 +1,20 @@
 (message "Loading custom rseed42 Emacs config...")
+;-------------------------------------------------------------------------------
 ; Do not create backup files. Using Git after all...
-(message "Disable auto backups")
+;-------------------------------------------------------------------------------
+;(message "Disable auto backups")
 (setq make-backup-files nil)
+;(message "[OK]")
+
+;-------------------------------------------------------------------------------
+; Set up local package directory
+;-------------------------------------------------------------------------------
+(add-to-list 'load-path "~/.emacs.d/local/")
+
+;-------------------------------------------------------------------------------
+; Load the package manager package.el prior to anything else
+;-------------------------------------------------------------------------------
+(require 'package)
+(package-initialize)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/") t)
