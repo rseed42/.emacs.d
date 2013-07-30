cfc1cbb9799457e6f6a4a0d6a3490781b7617942
Randall Seethe <rseed42@gmail.com>
2013-07-30 19:47:45 +0200

    Do not show startup messages

--- init.el
+++ init.el
@@ -1,3 +1,17 @@
+; Do not display tips on startup
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(inhibit-startup-screen t))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ )
+;-------------------------------------------------------------------------------
 (message "Loading custom rseed42 Emacs config...")
 ;-------------------------------------------------------------------------------
 ; Do not create backup files. Using Git after all...
