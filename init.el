dbc7314f9844f4492a036b668ae0da26c3f7508b
Randall Seethe <rseed42@gmail.com>
2013-08-24 23:14:24 +0200

    Experimental interface enhancements

--- init.el
+++ init.el
@@ -23,6 +23,45 @@
 ; Enable ansi color
 (require 'ansi-color)
 ;-------------------------------------------------------------------------------
+; Interface enhancement
+;-------------------------------------------------------------------------------
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit autoface-default :strike-through nil :underline nil :slant normal :weight normal :height 120 :width normal :family "monaco"))))
+ '(column-marker-1 ((t (:background "red"))))
+ '(diff-added ((t (:foreground "cyan"))))
+ '(flymake-errline ((((class color) (background light)) (:background "Red"))))
+ '(font-lock-comment-face ((((class color) (min-colors 8) (background light)) (:foreground "red"))))
+ '(fundamental-mode-default ((t (:inherit default))))
+ '(highlight ((((class color) (min-colors 8)) (:background "white" :foreground "magenta"))))
+ '(isearch ((((class color) (min-colors 8)) (:background "yellow" :foreground "black"))))
+ '(linum ((t (:foreground "black" :weight bold))))
+ '(region ((((class color) (min-colors 8)) (:background "white" :foreground "magenta"))))
+ '(secondary-selection ((((class color) (min-colors 8)) (:background "gray" :foreground "cyan"))))
+ '(show-paren-match ((((class color) (background light)) (:background "black"))))
+ '(vertical-border ((t nil)))
+)
+;-------------------------------------------------------------------------------
+; Better key bindings
+;-------------------------------------------------------------------------------
+;(load "defuns-config.el")
+;(fset 'align-equals "\C-[xalign-regex\C-m=\C-m")
+;(global-set-key "\M-=" 'align-equals)
+;(global-set-key "\C-x\C-m" 'execute-extended-command)
+;(global-set-key "\C-c;" 'comment-or-uncomment-region)
+;(global-set-key "\M-n" 'next5)
+;(global-set-key "\M-p" 'prev5)
+;(global-set-key "\M-o" 'other-window)
+;(global-set-key "\M-i" 'back-window)
+;(global-set-key "\C-z" 'zap-to-char)
+;(global-set-key "\C-h" 'backward-delete-char)
+;(global-set-key "\M-d" 'delete-word)
+;(global-set-key "\M-h" 'backward-delete-word)
+;(global-set-key "\M-u" 'zap-to-char)
+;-------------------------------------------------------------------------------
 ; Set up local package directory
 ;-------------------------------------------------------------------------------
 (add-to-list 'load-path "~/.emacs.d/local/")
