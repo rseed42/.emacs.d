a84b02f308d920984c03c5a1211a81c7c8673a53
Randall Seethe <rseed42@gmail.com>
2013-08-24 23:19:20 +0200

    enabled alternative key bindings

--- init.el
+++ init.el
@@ -47,24 +47,25 @@
 ;-------------------------------------------------------------------------------
 ; Better key bindings
 ;-------------------------------------------------------------------------------
-;(load "defuns-config.el")
-;(fset 'align-equals "\C-[xalign-regex\C-m=\C-m")
-;(global-set-key "\M-=" 'align-equals)
-;(global-set-key "\C-x\C-m" 'execute-extended-command)
-;(global-set-key "\C-c;" 'comment-or-uncomment-region)
-;(global-set-key "\M-n" 'next5)
-;(global-set-key "\M-p" 'prev5)
-;(global-set-key "\M-o" 'other-window)
-;(global-set-key "\M-i" 'back-window)
-;(global-set-key "\C-z" 'zap-to-char)
-;(global-set-key "\C-h" 'backward-delete-char)
-;(global-set-key "\M-d" 'delete-word)
-;(global-set-key "\M-h" 'backward-delete-word)
-;(global-set-key "\M-u" 'zap-to-char)
+(add-to-list 'load-path "~/.emacs.d/local/")
+(load "defuns-config.el")
+(fset 'align-equals "\C-[xalign-regex\C-m=\C-m")
+(global-set-key "\M-=" 'align-equals)
+(global-set-key "\C-x\C-m" 'execute-extended-command)
+(global-set-key "\C-c;" 'comment-or-uncomment-region)
+(global-set-key "\M-n" 'next5)
+(global-set-key "\M-p" 'prev5)
+(global-set-key "\M-o" 'other-window)
+(global-set-key "\M-i" 'back-window)
+(global-set-key "\C-z" 'zap-to-char)
+(global-set-key "\C-h" 'backward-delete-char)
+(global-set-key "\M-d" 'delete-word)
+(global-set-key "\M-h" 'backward-delete-word)
+(global-set-key "\M-u" 'zap-to-char)
 ;-------------------------------------------------------------------------------
 ; Set up local package directory
 ;-------------------------------------------------------------------------------
-(add-to-list 'load-path "~/.emacs.d/local/")
+;(add-to-list 'load-path "~/.emacs.d/local/")
 ; Enable smooth scrolling
 (require 'smooth-scrolling)
 ;-------------------------------------------------------------------------------
