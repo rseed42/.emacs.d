6cfe56c81496af1ae05991dea674a91cfbe96ae0
g6714 <venelin.petkov@man.eu>
2017-08-07 09:32:42 +0200

    Added new minibuffer font color for better resolution

--- init.el
+++ init.el
@@ -126,3 +126,15 @@
 ;(require 'jade-mode)
 ;(add-to-list 'auto-mode-alist '("\\.styl$" . sws-mode))
 ;(add-to-list 'auto-mode-alist '("\\.jade$" . jade-mode))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(minibuffer-prompt ((t (:foreground "color-27")))))
